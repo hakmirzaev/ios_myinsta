788cd9b30d2614b545f998edd652251bd3783545
Bekhruzjon Hakmirzaev <hakmirzaevbekhruzjon@gmail.com>
2023-02-08 16:20:41 +0500

    Initial Commit

---
+++ ios_myinsta/ios_myinstaApp.swift
@@ -0,0 +1,20 @@
+//
+//  ios_myinstaApp.swift
+//  ios_myinsta
+//
+//  Created by Bekhruz Hakmirzaev on 08/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_myinstaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
